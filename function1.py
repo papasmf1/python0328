dd3eefc420b0f07ee63dbc183d28280fe9f58a0f
papasmf1 <papasmf1@gmail.com>
2022-03-28 16:30:32 +0900

    함수 연습

--- function1.py
+++ function1.py
@@ -1,5 +1,3 @@
-# function1.py 
-
 #함수를 정의
 def times(a,b):
     return a*b 
@@ -7,3 +5,24 @@ def times(a,b):
 #함수를 호출
 print(times(2,3))
 
+#불변형식과 가변형식 
+a = 1.2
+print(id(a))
+
+a = 2.3
+print(id(a))
+
+print("---가변형식---")
+lst = [1,2,3]
+print(id(lst))
+lst.append(4)
+print(id(lst))
+
+#함수를 정의
+def change(x):
+    x[0] = "H"
+
+#호출
+wordlist = ["J","A","M"]
+change(wordlist)
+print("함수 호출후:", wordlist)
