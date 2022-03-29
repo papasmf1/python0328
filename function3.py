0900fc0b51785aa0a825248e64a5e45a942e5736
papasmf1 <papasmf1@gmail.com>
2022-03-29 09:38:02 +0900

    함수 완료

--- function3.py
+++ function3.py
@@ -15,4 +15,24 @@ def union(*ar):
 print(union("HAM","EGG"))
 print(union("HAM","EGG","SPAM"))
 
+#정의되지 않은 인자
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL 
+
+#호출
+print(userURIBuilder("credu.com","80",id="kim", age="30"))
+print(userURIBuilder("credu.com","80",id="kim", age="30",name="mike"))
+
+#람다 함수(이름이 없다!)
+g = lambda x,y:x*y
+print(g(3,4))
+print(g(5,6))
+
+print((lambda x:x*x)(3))
+
+print(globals())
+
 
