3b445f69b69eb0dfc33c3353fc39675c383863db
papasmf1 <papasmf1@gmail.com>
2022-03-29 14:50:34 +0900

    상속과 다형성 연습

--- 상속01.py
+++ 상속01.py
@@ -1,22 +1,33 @@
+#부모 클래스 정의 
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
         self.phoneNumber = phoneNumber
-
     def printInfo(self):
-        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, 
+            self.phoneNumber))
 
+#자식 클래스 정의
 class Student(Person):
+    #멤버변수가 추가
+    #상속받은 메서드를 재정의(덮어쓰기, override)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #명시적으로 부모 생성자 호출
+        Person.__init__(self, name, phoneNumber)
         self.subject = subject
         self.studentID = studentID
+    #상속받아서 재정의(부모의 코드, 자식의 코드)
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, 
+            self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(self.subject, 
+            self.studentID))    
 
-
+#인스턴스 생성
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+s = Student("이순신", "010-111-1234", "소프트웨어공학", "2011")
+p.printInfo()
+s.printInfo()
+
 
 
