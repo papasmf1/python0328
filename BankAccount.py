571f200901c61e31953bedced7d07b1eb2830b14
papasmf1 <papasmf1@gmail.com>
2022-03-29 14:23:44 +0900

    이름변경 연습

--- BankAccount.py
+++ BankAccount.py
@@ -2,19 +2,28 @@
 
 #은행의 계정을 표현한 클래스 
 class BankAccount:
+    #초기값을 셋팅(인스턴스)
     def __init__(self, id, name, balance):
-        self.id = id
-        self.name = name 
-        self.balance = balance 
+        #멤버 변수를 숨김(이름변경)
+        self.__id = id
+        self.__name = name 
+        self.__balance = balance 
+    #입금
     def deposit(self, amount):
-        self.balance += amount 
+        self.__balance += amount 
+    #출금
     def withdraw(self, amount):
-        self.balance -= amount
+        self.__balance -= amount
+    #문자열을 리턴(ToString()메서드)
     def __str__(self):
-        return "{0} , {1} , {2}".format(self.id, \
-            self.name, self.balance)
+        return "{0} , {1} , {2}".format(self.__id, \
+            self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
 account1.withdraw(3000)
 print(account1)
+#print(account1.__balance)
+#_클래스명__변수명(이름변경):백도어 
+print(account1._BankAccount__balance)
+
--- 생성자소멸자01.py
+++ 생성자소멸자01.py
@@ -1,8 +1,17 @@
 # -*- 생성자와 소멸자 -*-
 class MyClass:
+    #생성자(초기화)
     def __init__(self, value):
         self.value = value
         print("Instace is created! value = ", value)
+    #소멸자(가장 마지막 정리, 청소)
     def __del__(self):
         print("Instance is deleted!")
 
+
+#인스턴스 생성
+my = MyClass(5)
+#파이썬에서 가비지 컬렉션 서비스를 사용(자동 수거 서비스)
+#del my 
+
+print("전체 코드 실행 종료")
