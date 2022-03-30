7d7d09e034778723dab84aee86584d104f8d4d84
papasmf1 <papasmf1@gmail.com>
2022-03-30 10:46:38 +0900

    파일 연습3

--- DemoFile.py
+++ DemoFile.py
@@ -28,11 +28,24 @@ f.write("한글데이터\nabcd\n1234\n")
 f.close()
 print(f.closed)
 
-#파일을 읽기
-f = open("c:\\work\\demo.txt", "rt", encoding="utf-8")
+#파일을 읽기(raw string 표기법:가공하지 않은 문자열 처리)
+#한근태 작가의 몸이 먼저다~, 일생에 한번 고수를 만나라~ 
+f = open(r"c:\work\demo.txt", "rt", encoding="utf-8")
 #첫줄부터 끝까지 읽어서 str로 리턴 
 result = f.read()
 print(result)
+print("어디쯤:", f.tell())
+#처음으로 다시 리셋
+f.seek(0)
+#파일이 1GB정도의 큰규모라면? 서버의 활동을 기록한 로그파일(Text)
+print(f.readline(), end="")
+print(f.readline(), end="")
+
+#리스트로 받기
+f.seek(0)
+result = f.readlines()
+print(result)
+
 f.close()
 
 
