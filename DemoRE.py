4af10b70ebae5b4bda08bae0d2986df6a5f7345e
papasmf1 <papasmf1@gmail.com>
2022-03-30 13:19:22 +0900

    정규 표현식

---
+++ DemoRE.py
@@ -0,0 +1,16 @@
+# DemoRE.py
+import re 
+
+#print(dir(re))
+
+#서치는 처음부터 끝까지 검색
+result = re.search("[0-9]*th", "  35th")
+print(result)
+#처음을 보고 바로 포기 
+result = re.match("[0-9]*th", "  35th")
+print(result)
+#print(result.group())
+
+print(bool(re.search("apple", "this is apple")))
+print(bool(re.search("\d{4}", "올해는 2022년")))
+
