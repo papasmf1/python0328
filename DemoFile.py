6cdccc025be0ac50692a456c94aaa6f7f4d6fe55
papasmf1 <papasmf1@gmail.com>
2022-03-30 10:32:53 +0900

    파일 연습2

--- DemoFile.py
+++ DemoFile.py
@@ -17,10 +17,23 @@ print(url)
 print("--서식문자--")
 print("{0:x}".format(10))
 print("{0:b}".format(10))
-print("{0:e}".format(10))
+print("{0:e}".format(4/3))
 print("{0:f}".format(4/3))
 print("{0:.2f}".format(4/3))
 print("{0:,}".format(15000000))
+
+#파일에 쓰기
+f = open("c:\\work\\demo.txt", "wt", encoding="utf-8")
+f.write("한글데이터\nabcd\n1234\n")
+f.close()
+print(f.closed)
+
+#파일을 읽기
+f = open("c:\\work\\demo.txt", "rt", encoding="utf-8")
+#첫줄부터 끝까지 읽어서 str로 리턴 
+result = f.read()
+print(result)
+f.close()
 
 
 
